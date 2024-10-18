307a3910d8c462254fa53b3ee064ba8725548611
dukenguyenxyz <duke.nguyen.xyz@gmail.com>
2024-10-18 07:33:36 +0000

    feat: latest version running but not god results

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+# UPDATE LOGGING AND SAVING MODELS
+# SAVE ALL THE PARAMETERS FOR EACH TRAINING ITERATION
+# training logs are not reported
+
 import argparse
 import os, sys
 import os.path as osp
@@ -13,6 +17,7 @@ import random, pdb, math, copy
 from tqdm import tqdm
 from scipy.spatial.distance import cdist
 from sklearn.metrics import confusion_matrix
+import time
 
 import evaluate
 
@@ -37,29 +42,32 @@ def set_seed(seed):
     random.seed(seed)
 
 def obtain_label(inputs_, model, args):
-    start_test = True
-
     with torch.no_grad():
         loader = inputs_
-        
-        # print('HERE')
-        # print(inputs_.keys())
 
         inputs = inputs_['input_ids'].to(args.device)
+
+        # ACTUAL LABEL
         labels = inputs_['labels'].to(args.device)
 
         raw_outputs = model(inputs)
 
         all_fea = raw_outputs.hidden_states[-1][:, 0, :].float()#.cpu()
+
+        # PREDICTED LABEL
         all_output = raw_outputs.logits.float()#.cpu()
         all_label = labels.float()
 
     all_output = nn.Softmax(dim=1)(all_output)
+
     ent = torch.sum(-all_output * torch.log(all_output + args.epsilon), dim=1)
     unknown_weight = 1 - ent / np.log(args.class_num)
     _, predict = torch.max(all_output, 1)
 
-    accuracy = torch.sum(torch.squeeze(predict).float() == all_label).item() / float(all_label.size()[0])
+    old_predict = predict.detach().clone().cpu().numpy()
+
+    # LABEL
+    acc_pre_tta = torch.sum(torch.squeeze(predict).float() == all_label).item() / float(all_label.size()[0])
     if args.distance == 'cosine':
         all_fea = torch.cat((all_fea, torch.ones(all_fea.size(0), 1, device = all_fea.device)), 1)
         all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
@@ -71,7 +79,9 @@ def obtain_label(inputs_, model, args):
     aff = all_output.float().cpu().numpy()
     # aff = all_output.float().numpy()
 
+    # re-labelling via centroids
     for _ in range(2):
+        # initc is the centroid for each class
         initc = aff.transpose().dot(all_fea)
         initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
         if torch.is_tensor(predict):
@@ -87,8 +97,14 @@ def obtain_label(inputs_, model, args):
 
         aff = np.eye(K)[predict]
 
-    acc = np.sum(predict == all_label.float().cpu().numpy()) / len(all_fea)
-    log_str = 'Accuracy = {:.2f}% -> {:.2f}%'.format(accuracy * 100, acc * 100)
+    # ASSIGN NEW LABEL
+    acc_post_tta = np.sum(predict == all_label.float().cpu().numpy()) / len(all_fea)
+
+    # THEY ARE FREAKING EQUAL
+    print("ARE TWO PREIDCT EQUAL: ", (old_predict==predict).all())
+
+    # THIS SHOULD BE DIFFERENT BUT POSSIBLE THAT IT IS NOT DIFFERENT INTERVAL NEEDS TO INCREASE
+    log_str = 'Accuracy = {:.2f}% -> {:.2f}%'.format(acc_pre_tta * 100, acc_post_tta * 100)
 
     args.out_file.write(log_str + '\n')
     args.out_file.flush()
@@ -97,34 +113,49 @@ def obtain_label(inputs_, model, args):
     return predict.astype('int')
 
 class CustomTrainer(Trainer):
-    def __init__(self, device=None, *args, **kwargs):
+    def __init__(self, extra_args = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.device = device
+        # can incorporate this into args or kwargs
+        # self.device = self.args.device
+        # self.interval = self.args.interval
+        # print(self.args)
+        # exit(0)
+        self.extra_args = extra_args
+        self.device = extra_args.device
+        self.interval_iter = self.args.num_train_epochs // extra_args.interval
+
+        # self.mem_label = None
 
     def compute_loss(self, model, inputs, return_outputs=False):
+        # current_iter = self.state.global_step
+        # print(current_iter)
+        # if current_iter % self.interval_iter == 0 and self.extra_args.cls_par > 0:
+            # pseudo_label
+        model.eval()
+        mem_label = obtain_label(inputs, model, self.extra_args)
+        mem_label = torch.from_numpy(mem_label).to(self.device)
+        model.train()
+
         inputs = {k: v.to(self.device) for k, v in inputs.items()} #  if k in ["input_ids", "attention_mask", "label"]
 
         # Forward pass
         outputs = model(**inputs)
         logits = outputs.logits
 
-        # pseudo_label
-        mem_label = obtain_label(inputs, model, args)
-        mem_label = torch.from_numpy(mem_label).to(self.device)
-
         # Custom loss calculation
         classifier_loss = nn.CrossEntropyLoss()(logits, mem_label)
 
-        classifier_loss *= args.cls_par
-        if args.ent:
+        classifier_loss *= self.extra_args.cls_par
+        if self.extra_args.ent:
             softmax_out = nn.Softmax(dim=1)(logits)
             entropy_loss = torch.mean(Entropy(softmax_out))
-            if args.gent:
+            if self.extra_args.gent:
                 msoftmax = softmax_out.mean(dim=0)
-                gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + args.epsilon))
+                gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + self.extra_args.epsilon))
                 entropy_loss -= gentropy_loss
-            im_loss = entropy_loss * args.ent_par
+            im_loss = entropy_loss * self.extra_args.ent_par
             classifier_loss += im_loss
+
         # return (classifier_loss, logits) if return_outputs else classifier_loss
 
         # https://discuss.huggingface.co/t/evalprediction-has-an-unequal-number-of-label-ids-and-predictions/10557/4
@@ -132,7 +163,7 @@ class CustomTrainer(Trainer):
 
 def train_target(args):
     # Load GLUE dataset
-    dataset = load_from_disk(f"./datasets/{args.dset}_validation").select(range(20))
+    dataset = load_from_disk(f"./datasets/{args.dset}_validation")#.select(range(20))
 
     args.savename = 'par_' + str(args.cls_par)
     args.out_file = open(osp.join(args.output_dir, 'log_' + args.savename + '.txt'), 'w')
@@ -140,7 +171,13 @@ def train_target(args):
     # Load Roberta Model and Tokenizer
     # args.model_name
     tokenizer = RobertaTokenizer.from_pretrained('./models/train/roberta_tokenizer_rte')
-    model = RobertaForSequenceClassification.from_pretrained('./models/train/roberta_model_rte', output_hidden_states=True).to(args.device)
+
+    model = RobertaForSequenceClassification.from_pretrained('./models/train/roberta_model_rte', output_hidden_states=True)
+    args.device = torch.device(f'cuda:0')
+    model.to(args.device)
+
+    # model = nn.DataParallel(RobertaForSequenceClassification.from_pretrained('./models/train/roberta_model_rte', output_hidden_states=True), device_ids=[1,2])
+    # args.device = model.device
 
     # Freeze the model here
     param_group = []
@@ -195,7 +232,10 @@ def train_target(args):
         return metric.compute(predictions=predictions, references=labels)
 
     trainer = CustomTrainer(
-        device = args.device,
+        # device = args.device,
+        # interval = args.interval,
+
+        extra_args=args,
         model=model,
         args=training_args,
         train_dataset=encoded_dataset,
@@ -208,33 +248,35 @@ def train_target(args):
     trainer.train()
     trainer.evaluate()
 
-    model.save_pretrained(f"./models/test/roberta_model_{args.dset}")
+    model.save_pretrained(f"./models/test/roberta_model_{args.dset}_fullbatch")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SHOT adaptation for RoBERTa on GLUE tasks')
-    parser.add_argument('--gpu_id', type=str, default='0', help="GPU id to use")
+    parser.add_argument('--gpu_id', type=str, default='2', help="GPU id to use")
     parser.add_argument('--dset', type=str, default='rte', choices=["sst2", "mnli", "mrpc", "cola", "stsb", "qqp", "rte", "wnli", "qnli"], help="GLUE task name")
 
     # This needs to be the trained model
     parser.add_argument('--model_name', type=str, default='roberta-base', help="Pre-trained RoBERTa model")
     parser.add_argument('--model_output_size', type=int, default=768, help='The size of the output of your model, RoBERTA would be 768.')
     parser.add_argument('--max_epoch', type=int, default=50, help="Number of training epochs")
-    parser.add_argument('--batch_size', type=int, default=4, help="Batch size for training")
+    parser.add_argument('--batch_size', type=int, default=16, help="Batch size for training")
     parser.add_argument('--lr', type=float, default=1e-5, help="Learning rate")
     parser.add_argument('--distance', type=str, default='cosine', choices=["euclidean", "cosine"])  
+    parser.add_argument('--interval', type=int, default=15)
 
     parser.add_argument('--gent', type=bool, default=True)
     parser.add_argument('--ent', type=bool, default=True)
-    parser.add_argument('--threshold', type=int, default=0)
     parser.add_argument('--cls_par', type=float, default=0.3)
     parser.add_argument('--ent_par', type=float, default=1.0)
     parser.add_argument('--lr_decay1', type=float, default=0.1)
     parser.add_argument('--lr_decay2', type=float, default=1.0)
+
     parser.add_argument('--epsilon', type=float, default=1e-5)
+    parser.add_argument('--threshold', type=int, default=0)
    
     parser.add_argument('--max_length', type=int, default=128, help="Maximum sequence length")
     parser.add_argument('--weight_decay', type=float, default=0.01, help="Weight decay for the optimizer")
-    parser.add_argument('--logging_dir', type=str, default='./logs', help="Directory for storing logs")
+    parser.add_argument('--logging_dir', type=str, default=f"./logs/test/{int(time.time())}", help="Directory for storing logs")
     parser.add_argument('--logging_steps', type=int, default=500, help="Steps for logging")
     parser.add_argument('--save_total_limit', type=int, default=1, help="Limit the total number of checkpoints")
     parser.add_argument('--output_dir', type=str, default='./results', help="Directory for saving model checkpoints")
@@ -251,13 +293,15 @@ if __name__ == "__main__":
         
     # 'cola', 'mnli', 'qnli', 'rte', 'qqp', 'sst2', 'sts-b', 'all'
 
-    # if torch.backends.mps.is_built:
-    #     args.device = torch.device('mps')
-    if torch.cuda.is_available():
-        args.device = torch.cuda.current_device()
-        # args.device = torch.device('cuda:3')
-    else:
-        args.device = torch.device("cpu")
+    # # if torch.backends.mps.is_built:
+    # #     args.device = torch.device('mps')
+    # if torch.cuda.is_available():
+    #     # args.device = torch.cuda.current_device()
+    #     # args.device = torch.device("cuda:3,4")
+    #     args.device = torch.device('cuda:3')
+    #     # args.device = 'parallel'
+    # else:
+    #     args.device = torch.device("cpu")
 
     args.num_input, args.class_num = get_class(args.dset)
     
--- train.py
+++ train.py
@@ -1,24 +1,40 @@
 import torch
-from transformers import RobertaForSequenceClassification, RobertaTokenizer, Trainer, TrainingArguments
+import torch.nn as nn
+from transformers import RobertaForSequenceClassification, RobertaTokenizer, Trainer, TrainingArguments, DataCollatorWithPadding
 from datasets import load_dataset, load_from_disk
 import evaluate
+import os
+import time
 
 task = 'rte'
 
-train_dataset = load_dataset("glue", task, split='train').select(range(20))
+train_dataset = load_dataset("glue", task, split='train')#.select(range(20))
 
 #.select(range(20))
 
 # Load the tokenizer and model
 tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+
 model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=2) #output_hidden_states=True
+
+# device_ids = [0,1]
+# os.environ["CUDA_VISIBLE_DEVICES"] = '0,1'
+# model = torch.nn.DataParallel(model, device_ids=device_ids)
+# model.to(f'cuda:{device_ids[0]}')
+# model.cuda(device_ids[0])
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '3'
+device = torch.device(f'cuda:0')
+model.to(device)
 
 def tokenize_function(examples):
     return tokenizer(examples["sentence1"], examples["sentence2"], padding="max_length", truncation=True)
 
 tokenized_train_datasets = train_dataset.map(tokenize_function, batched=True)
 
-validation_dataset = load_from_disk(f"./datasets/{task}_validation").select(range(20))
+validation_dataset = load_from_disk(f"./datasets/{task}_validation")#.select(range(20))
 tokenized_validation_datasets = validation_dataset.map(tokenize_function, batched=True)
 
 # Set format for PyTorch
@@ -37,26 +53,47 @@ def compute_metrics(eval_pred):
     predictions = torch.argmax(torch.tensor(logits), dim=-1)
     return metric.compute(predictions=predictions, references=labels)
 
+class CustomTrainer(Trainer):
+    def __init__(self, label_smoothing=0.5, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loss = nn.CrossEntropyLoss(label_smoothing=label_smoothing)
+        
+    def compute_loss(self, model, inputs, return_outputs=False):
+        inputs = {k: v.to(model.device) for k, v in inputs.items()} #  if k in ["input_ids", "attention_mask", "label"]
+
+        # Forward pass
+        outputs = model(**inputs)
+        logits = outputs.logits
+        # logits = outputs
+
+        # Custom loss calculation
+        classifier_loss = self.loss(logits, inputs['labels'])
+
+        # https://discuss.huggingface.co/t/evalprediction-has-an-unequal-number-of-label-ids-and-predictions/10557/4
+        return (classifier_loss, {"label": logits}) if return_outputs else classifier_loss
+
 # Set training arguments
 training_args = TrainingArguments(
     output_dir="./results",
     eval_strategy="epoch",
     learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=50,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=5,
     weight_decay=0.01,
-    logging_dir="./logs",
+    logging_dir=f"./logs/train/{int(time.time())}",
     logging_steps=10,
 )
 
 # Initialize the Trainer
-trainer = Trainer(
+trainer = CustomTrainer(
+    label_smoothing=0.1,
     model=model,
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=validation_dataset,
     compute_metrics=compute_metrics,
+    data_collator=data_collator
 )
 
 # Train the model
